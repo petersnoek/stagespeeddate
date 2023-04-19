ffeb76b0d2319d50d7a278a0fd52b9508e7bd8bd
Joshua <Doerak1aug@gmail.com>
2023-04-19 14:37:43 +0200

    #0 V2.0
    intergrated laravel/ui with One UI

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -72,9 +72,90 @@
 
       'sidebar-dark page-header-dark dark-mode'   Enable dark mode (light sidebar/header is not supported with dark mode)
   -->
-  <div id="page-container">
+  <div id="page-container" class="page-header-fixed">
+
+    <header id="page-header" class="shadow-sm">
+      <!-- Header Content -->
+      <div class="content-header">
+        <!-- Left Section -->
+        <div class="d-flex align-items-center">     
+          <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+          </a>  
+        </div>
+        <!-- END Left Section -->
+
+        <!-- Right Section -->
+        <div class="d-flex align-items-center">
+          <!-- User Dropdown -->
+          @guest
+            @if (Route::has('login'))
+                <button type="button" class="btn btn-sm btn-alt-secondary d-flex align-items-center mx-2">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </button>
+            @endif
+
+            @if (Route::has('register'))
+                <div type="button" class="btn btn-sm btn-alt-secondary d-flex align-items-center mx-2">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </div>
+            @endif
+          @else
+            <div class="dropdown d-inline-block ms-2">
+              <button type="button" class="btn btn-sm btn-alt-secondary d-flex align-items-center" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img class="rounded-circle" src="{{ asset('media/avatars/avatar10.jpg') }}" alt="Header Avatar" style="width: 21px;">
+                <span class="d-none d-sm-inline-block ms-2">{{ Auth::user()->name }}</span>
+                <i class="fa fa-fw fa-angle-down d-none d-sm-inline-block ms-1 mt-1"></i>
+              </button>
+              <div class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0" aria-labelledby="page-header-user-dropdown">
+                <div class="p-3 text-center bg-body-light border-bottom rounded-top">
+                  <img class="img-avatar img-avatar48 img-avatar-thumb" src="{{ asset('media/avatars/avatar10.jpg') }}" alt="">
+                  <p class="mt-2 mb-0 fw-medium">{{ Auth::user()->name }}</p>
+                  <p class="mb-0 text-muted fs-sm fw-medium">{{ Auth::user()->email }}</p>
+                </div>
+                <div class="p-2">
+                  <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
+                    <span class="fs-sm fw-medium">Profile</span>
+                    {{-- <span class="badge rounded-pill bg-primary ms-2">1</span> --}}
+                  </a>
+                </div>
+                <div role="separator" class="dropdown-divider m-0"></div>
+                <div class="p-2">
+                  {{-- <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
+                    <span class="fs-sm fw-medium">Log Out</span>
+                  </a> --}}
+                  <a class="dropdown-item d-flex align-items-center justify-content-between"
+                    onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">
+                    <span class="fs-sm fw-medium">{{ __('Log Out') }}</span>
+                  </a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                  </form>
+                </div>
+              </div>
+            </div>
+          @endguest
+          <!-- END User Dropdown -->
+        </div>
+        <!-- END Right Section -->
+      </div>
+      <!-- END Header Content -->
+
+      <!-- Header Loader -->
+      <!-- Please check out the Loaders page under Components category to see examples of showing/hiding it -->
+      <div id="page-header-loader" class="overlay-header bg-body-extra-light">
+        <div class="content-header">
+          <div class="w-100 text-center">
+            <i class="fa fa-fw fa-circle-notch fa-spin"></i>
+          </div>
+        </div>
+      </div>
+      <!-- END Header Loader -->
+    </header>
+
     <!-- Main Container -->
-    <main id="main-container">
+    <main id="main-container" class="pt-7">
       @yield('content')
     </main>
     <!-- END Main Container -->
