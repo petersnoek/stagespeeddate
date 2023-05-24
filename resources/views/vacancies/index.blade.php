d06aa9829c44cde5798778905696dda64fd3d885
Joshua <Doerak1aug@gmail.com>
2023-05-24 11:58:15 +0200

    devBugs1 #4.0

--- resources/views/vacancies/index.blade.php
+++ resources/views/vacancies/index.blade.php
@@ -7,10 +7,18 @@
       <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center py-2">
         <div class="flex-grow-1">
           <h1 class="h3 fw-bold mb-2">
+          @if($vacancies->first()->company->name == $vacancies->last()->company->name)
+            {{$vacancies->first()->company->name}}
+          @else
+            Alle
+          @endif
             Vacatures
           </h1>
           <h2 class="fs-base lh-base fw-medium text-muted mb-0">
           Een overzicht van alle beschikbare vacatures
+          @if($vacancies->first()->company->name == $vacancies->last()->company->name)
+            bij {{$vacancies->first()->company->name}}
+          @endif
           </h2>
         </div>
         <nav class="flex-shrink-0 mt-3 mt-sm-0 ms-sm-3" aria-label="breadcrumb">
