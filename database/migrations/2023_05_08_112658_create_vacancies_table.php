2cf73f29d988b909038ede5a46adf6f60609a754
LeviHoevers <99065078@mydavinci.nl>
2023-06-06 13:56:50 +0200

    #6 v1.3

--- database/migrations/2023_05_08_112658_create_vacancies_table.php
+++ database/migrations/2023_05_08_112658_create_vacancies_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->tinyText('bio')->nullable();
             $table->text('description')->nullable();
-            $table->boolean('available')->nullable();
+            $table->boolean('available');
             $table->timestamps();
         });
     }
